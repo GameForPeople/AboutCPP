fc9eae349fbe3539e93dc0307d35128f9d455bb6
GameForPeople <koreagamemaker@gmail.com>
2018-10-09 20:50:18 +0900

    Level1_자릿수_더하기
    Level1_자릿수_더하기

--- AboutCpp/AboutCpp/Algorithm/Programmers/Level1/자릿수_더하기.h
+++ AboutCpp/AboutCpp/Algorithm/Programmers/Level1/자릿수_더하기.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+	int answer{};
+	int nBuffer = n;
+
+	for (int i = 1; n / i > 0; i *= 10) {
+		answer += nBuffer % 10;
+		nBuffer /= 10;
+	}
+
+	return answer;
+}
