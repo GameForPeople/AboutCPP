e17d76d37b168e1b54228ba30ef8843f5925f1da
GameForPeople <koreagamemaker@gmail.com>
2018-10-10 23:10:53 +0900

    Level1_약수의_합
    Level1_약수의_합

--- AboutCpp/AboutCpp/Algorithm/Programmers/Level1/약수의_합.h
+++ AboutCpp/AboutCpp/Algorithm/Programmers/Level1/약수의_합.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer{};
+
+	for (int i = 1; i < n; ++i) {
+		if (n % i == 0) 
+		{
+			answer += i;
+		}
+	}
+
+	return answer + n;
+}
