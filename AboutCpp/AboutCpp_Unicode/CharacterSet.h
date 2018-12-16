e6d941b3227a1807131b6ce6b4db12ac390644f9
GameForPeople <koreagamemaker@gmail.com>
2018-12-16 23:09:27 +0900

    Create "Unicode Project"

---
+++ AboutCpp/AboutCpp_Unicode/CharacterSet.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include <string>
+
+namespace CharacterSet
+{
+	class TestCharacterSet
+	{
+	public:
+		void operator()()
+		{
+			using namespace std;
+
+			std::wstring testString;
+		}
+	};
+}
